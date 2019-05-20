e4f3b9ccdfdf464478f303bfb87005544aeb9d5e
bishwajeetgupta <50728266+bishwajeetgupta@users.noreply.github.com>
2019-05-20 18:07:50 +0530

    redesign
    "redesign vendor page and add menu into header"

--- local/resources/views/vendor.blade.php
+++ local/resources/views/vendor.blade.php
@@ -32,472 +32,164 @@
 
 
 
+
+<div class="clearfix"></div>
 <div class="clearfix"></div>
 
 
-
-
-
-<div class="video">
-	<div class="clearfix"></div>
-	<div class="">
-
-
-
-
-
-
-
-
-
-
-		<?php if($shopcount==1){?>
-		<div class="row profile shop">
-			<div class="fb-profile">
-				<?php
-				$shopheader="/shop/";
-				$path ='/local/images'.$shopheader.$shop[0]->cover_photo;
-				if($shop[0]->cover_photo!=""){
-				?>
-				<img align="left" class="fb-image-lg" src="<?php echo $url.$path;?>" alt="cover banner"/>
-				<?php } else { ?>
-				<img align="left" class="fb-image-lg" src="<?php echo $url.'/local/images/no-image-big.jpg';?>" alt="cover banner"/>
-				<?php } ?>
-
-				<?php $shopphoto="/shop/";
-				$paths ='/local/images'.$shopphoto.$shop[0]->profile_photo;
-				if($shop[0]->profile_photo!=""){?>
-				<img align="left" class="fb-image-profile thumbnail" src="<?php echo $url.$paths;?>" alt="Profile Photo"/>
-				<?php } else { ?>
-				<img align="left" class="fb-image-profile thumbnail customwidth" src="<?php echo $url.'/local/images/nophoto.jpg';?>" alt="Profile Photo"/>
-				<?php } ?>
-				<div class="fb-profile-text">
-					<h1 style="display:inline"><?php echo $shop[0]->shop_name;?></h1>
-
-					<input type="submit" name="#" id="#"  class="btn btn-bookmark" value="Pinned Seller"></a>
-
-					<p><?php echo $shop[0]->address;?></p>
-				</div>
-
-
-
-			</div>
-
-			<div class="container">
-				<div class="row">
-
-
-					<div class="col-md-12">
-
-
-
-						<div class="clearfix"></div>
-
-
-
-
-						<ul class="nav nav-tabs" id="myTab">
-							<li class="active"><a href="#sent" data-toggle="tab"><span class="lnr lnr-cog blok"></span> Services</a></li>
-							<li><a href="#inbox" data-toggle="tab"><span class="lnr lnr-user blok"></span> Profile</a></li>
-
-							<li><a href="#assignment" data-toggle="tab"><span class="lnr lnr-star blok"></span> Reviews</a></li>
-
-							<li><a href="#gallery" data-toggle="tab"><span class="lnr lnr-picture blok"></span> Gallery </a></li>
-
-							<li><a href="#contact" data-toggle="tab"><span class="lnr lnr-phone-handset blok"></span> Contact Vendor </a></li>
-
-						</ul>
-
-						<div class="tab-content">
-
-
-							<div class="tab-pane active" id="sent">
-
-								<div class="clearfix"></div>
-								<div class="col-md-12">
-
-									<?php foreach($viewservice as $sellerservice){?>
-
-
-
-									<div class="col-md-3">
-										<div class="services">
-											<div class="col-md-6">
-												<?php
-												$subservicephoto="/subservicephoto/";
-												$path ='/local/images'.$subservicephoto.$sellerservice->subimage;
-												if($sellerservice->subimage!=""){
-												?>
-												<img src="<?php echo $url.$path;?>" border="0" class="img-responsive imgradius" alt="">
-												<?php } else { ?>
-												<img src="<?php echo $url.'/local/images/noimage.jpg';?>" class="img-responsive imgradius" alt="">
-												<?php } ?>
-											</div>
-
-
-											<div class="col-md-6 nopadding">
-												<h4 class="customh4"><?php echo $sellerservice->subname;?></h4>
-
-												<h5 class="customh5"><i class="fa fa-info-circle yellows" aria-hidden="true"></i> <?php echo $sellerservice->price;?> <?php echo $setting[0]->site_currency;?> | <?php echo $sellerservice->time . " hr"; ?></h5>
-
-											</div>
-
-
-											<div class="col-md-12">
-												<a href="<?php echo $url;?>/booking/<?php echo $vendor;?>/<?php echo $sellerservice->subid;?>/<?php echo $userid;?>" class="radiusoff">
-													<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="booknow radiusoff" value="Book Now"></a>
-											</div>
-										</div>
-									</div>
-
-
-
-
-									<?php } ?>
-
-								</div>
-
-
-
-
-
-							</div>
-
-
-
-							<div class="tab-pane" id="inbox">
-								<div class="clearfix"></div>
-								<div class="col-md-12">
-									<div class="col-md-6">
-										<h3>Description</h3>
-										<p><?php echo $shop[0]->description;?></p><br/>
-									</div>
-									<div class="col-md-6 contact_address">
-										<h3>Contact Address</h3>
-										<p><span class="lnr lnr-map-marker"></span> <?php echo  ' '.$shop[0]->address;?><br>
-											<?php echo $shop[0]->city;?> - <?php echo $shop[0]->pin_code;?><br>
-											<?php echo $shop[0]->state;?><br>
-											<?php echo $shop[0]->country;?></p>
-										<p> <span class="lnr lnr-clock"></span> <?php echo $stime;?> - <?php echo $etime;?></p>
-									</div>
-								</div>
-
-								<div class="col-md-12">
-									<div class="col-md-6 working_day">
-										<h3>Shop Working Days</h3>
-										<p><?php for($i=0;$i<$lev;$i++){ if($sel[$i]=="0") echo "Sunday"; }?></p>
-										<p><?php for($i=0;$i<$lev;$i++){ if($sel[$i]=="1") echo "Monday"; }?></p>
-										<p><?php for($i=0;$i<$lev;$i++){ if($sel[$i]=="2") echo "Tuesday"; }?></p>
-										<p><?php for($i=0;$i<$lev;$i++){ if($sel[$i]=="3") echo "Wednesday"; }?></p>
-										<p><?php for($i=0;$i<$lev;$i++){ if($sel[$i]=="4") echo "Thursday"; }?></p>
-										<p><?php for($i=0;$i<$lev;$i++){ if($sel[$i]=="5") echo "Friday "; }?></p>
-										<p><?php for($i=0;$i<$lev;$i++){ if($sel[$i]=="6") echo "Saturday"; }?></p>
-
-									</div>
-
-									<div class="col-md-6">
-
-									</div>
-								</div>
-
-
-
-							</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-							<div class="tab-pane" id="assignment">
-								<div class="clearfix"></div>
-
-								<?php if($rating_count==0) {?>
-								<div class="row">
-									<div class="col-md-12">
-										<div class="rating">
-											No Reviews
-										</div>
-
-									</div>
-								</div>
-								<?php } else { ?>
-
-								<div class="row">
-									<div class="col-md-12">
-										<?php foreach($rating as $newrating){?>
-										<div class="rating">
-											<?php
-											if($newrating->rating=="")
-											{
-												$starpath = '/local/images/nostar.png';
-											}
-											else {
-												$starpath = '/local/images/'.$newrating->rating.'star.png';
-											}
-											?>
-											<img src="<?php echo $url.$starpath;?>" class="star_rates" alt="rated <?php if($newrating->rating==""){ echo "0"; } else { echo $newrating->rating; }?> stars" title="rated <?php if($newrating->rating==""){ echo "0"; } else { echo $newrating->rating; }?> stars" />  - &nbsp; <?php  echo $newrating->name;?>
-											<h4> <?php echo $newrating->comment; ?></h4>
-											<?php
-
-											?>
-
-										</div>
-										<?php } ?>
-
-									</div>
-								</div>
-
-								<?php } ?>
-
-
-
-
-							</div>
-
-
-
-
-
-
-
-
-
-							<div class="tab-pane" id="gallery">
-								<div class="clearfix"></div>
-								<div class="row">
-									<div class="col-md-12">
-
-										<?php foreach($viewgallery as $newgal){?>
-
-
-										<div class="col-md-3">
-											<?php
-											$galleryimg="/gallery/";
-											$path ='/local/images'.$galleryimg.$newgal->image;
-											if($newgal->image!=""){
-											?>
-
-											<a href="<?php echo $url.$path;?>" data-lightbox="image-1" ><img src="<?php echo $url.$path;?>" class="img-responsive" ></a>
-											<?php } else {?>
-											<img src="<?php echo $url.'/local/images/noimage.jpg';?>" class="img-responsive" >
-											<?php } ?>
-										</div>
-
-										<?php }?>
-									</div>
-								</div>
-
-							</div>
-
-
-
-
-
-							<div class="tab-pane" id="contact">
-								<div class="clearfix"></div>
-								<div class="row">
-									<div class="col-md-12">
-
-										<form class="" name="admin" id="formID" method="post" enctype="multipart/form-data" action="{{ route('vendor') }}">
-
-											{{ csrf_field() }}
-											<input type="hidden" id="vendor_id" name="vendor_id" value="<?php echo $shop_id; ?>">
-											<input type="hidden" id="vendor_email" name="vendor_email" value="<?php echo $vendor_email;?>">
-
-											<input type="hidden" id="admin_email" name="admin_email" value="<?php echo $admin_email;?>">
-
-											<div class="col-md-6">
-												<?php
-												if(Auth::check()) {
-													$logemail = Auth::user()->email;
-													$logname = Auth::user()->name;
-													$logphone =  Auth::user()->phone;
-												}
-												else
-												{
-													$logemail = "";
-													$logname = "";
-													$logphone =  "";
-												}
-												?>
-												<div class="form-group">
-													<label for="name" class="col-md-12">Your Name <span class="require">*</span></label>
-
-													<div class="col-md-12">
-														<input id="name" type="text" class="form-control validate[required] text-input" name="name" value="<?php echo $logname;?>" required autofocus>
-
-
-													</div>
-												</div>
-
-
-												<div class="form-group">
-													<label for="name" class="col-md-12">Your Email <span class="require">*</span></label>
-
-													<div class="col-md-12">
-														<input id="email" type="email" class="form-control validate[required,custom[email]] text-input" name="email" value="<?php echo $logemail;?>" required>
-
-
-													</div>
-												</div>
-
-											</div>
-
-
-
-											<div class="col-md-6">
-
-												<div class="form-group">
-													<label for="phone_no" class="col-md-12">Your Phone No <span class="require">*</span></label>
-
-													<div class="col-md-12">
-														<input id="phone_no" type="text" class="form-control validate[required] text-input" name="phone_no" value="<?php echo $logphone;?>" required>
-
-
-													</div>
-												</div>
-
-
-												<div class="form-group">
-													<label for="name" class="col-md-12">Your Message <span class="require">*</span></label>
-
-													<div class="col-md-12">
-														<textarea id="message" type="text" class="form-control validate[required] text-input" name="message" required></textarea>
-
-
-													</div>
-												</div>
-
-
-
-												<?php if(!empty($site_setting[0]->site_logo)){
-
-												?>
-
-												<input type="hidden" name="site_logo" value="<?php echo $url.'/local/images/settings/'.$site_setting[0]->site_logo;?>">
-
-												<?php } else { ?>
-
-												<input type="hidden" name="site_logo" value="">
-
-												<?php } ?>
-
-
-												<input type="hidden" name="site_name" value="<?php echo $site_setting[0]->site_name;?>">
-
-
-
-											</div>
-
-
-											<div class="col-md-6">
-
-												<?php if(Auth::check()) { ?>
-												<button type="submit" class="btn btn-success">
-													Submit
-												</button>
-												<?php } else { ?>
-												<button type="button" class="btn btn-success" onclick="alert('Login user only');">
-													Submit
-												</button>
-												<?php } ?>
-											</div>
-											<div class="col-md-6">
-											</div>
-
-
-
-
-
-
-										</form>
-
-
-
-									</div>
-								</div>
-
-							</div>
-
-
-
-
-
-						</div>
-
-
-
-
-
-
-
-						<div class="form-group">
-							<div class="row">
-								<div class="col-md-12">
-
-								</div>
-
-							</div>
-						</div>
-
-
-
-
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12 col-12 bg-company-info">
+
+	</div>
+</div>
+
+		<div class="container block-company">
+				<div class="row ">
+					<div class="col-md-2 col-4">
+						<img src="../img/banner.jpg" class="img-fluid company-profile"></div>
+					<div class="col-md-6 col-8">
+						<h2>Dummy India pvt ltd.</h2>
+						<p><strong>Location-</strong> KirtiNagar, Delhi 200 <strong>Profile View</strong></p>
+						<table class="text-center">
+							<tr>
+								<td><strong>Nature of Business</strong></td>
+								<td><strong>Year of Estblishment</strong></td>
+							</tr>
+							<tr>
+								<td>Manufecturer</td>
+								<td>2010</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td><strong>legar status of Firm</strong></td>
+								<td><strong>GST NO.</strong></td>
+							</tr>
+							<tr>
+								<td>Manufecturer</td>
+								<td>2010</td>
+								<td></td>
+							</tr>
+						</table>
+							<p>Concise description which, among other items of information, includes (1) firm's history, (2) number and quality of its human, financial, and physical resources (3) organizational and management structure, (4) past, current and anticipated performance, and (5) its reputation, and the standing of its goods or services.</p>
 					</div>
-				</div>
+					<div class="col-md-4 col-12">
+						<div class="well well-sm">
+							<div class="row">
+								<div class="col-xs-12 col-md-6 text-center">
+									<h1 class="rating-num">
+										4									</h1>
+									<div class="rating">
+
+										<img src="http://localhost/dealersahab/local/images/4star.png" alt="rated 4													stars" class="star_rates">
+									</div>
+									<div>
+										<span class="glyphicon glyphicon-user"></span>47 Rating
+										<button type="button" class="btn btn-warning" name="submit">View Detail</button>
+									</div>
+								</div>
+								<div class="col-xs-12 col-md-6">
+									<div class="row rating-desc">
+										<div class="col-xs-3 col-md-3 text-right">
+											<span class="glyphicon glyphicon-star"></span>4										</div>
+										<div class="col-xs-8 col-md-9">
+											<div class="progress progress-striped">
+												<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+													<span class="sr-only">80%</span>
+												</div>
+
+											</div>
+										</div>
+
+										<!-- end 1 -->
+									</div>
+									<!-- end row -->
+								</div>
+							</div>
+						</div>
+						<p><strong>Product Dealing-</strong> Compressor,ac Freez Motor</p>
+						<p><strong>Brand Comoany-</strong> Alg8i, Usha</p>
+					</div>
 
 
-			</div>
 
 
 
 
 
 
+	<div class="row">
 
+		<div class="col-md-6 company-info pb-20 pl-80 pr-20">
+			<h3><span class="pl-80">Reviews</span></h3>
+			<img src="../img/banner.jpg" alt="">
+			<p>Graphic Designer</p>
+			<span class="fa fa-star checked"></span>
+			<span class="fa fa-star checked"></span>
+			<span class="fa fa-star checked"></span>
+			<span class="fa fa-star "></span>
+			<span class="fa fa-star "></span>
+			<p>We have using ac brands of all types but it is my first time with hitach 1.5 ton it is a mind blowing air conditioner it has a superb cooling capacity.it cools our all the room very fast .now it was my loving brand.</p>
 
+			<img src="../img/banner.jpg" alt="">
+			<p>Logo Maker</p>
+			<span class="fa fa-star checked"></span>
+			<span class="fa fa-star checked"></span>
+			<span class="fa fa-star checked"></span>
+			<span class="fa fa-star checked"></span>
+			<span class="fa fa-star "></span>
+			<p>We have using ac brands of all types but it is my first time with hitach 1.5 ton it is a mind blowing air conditioner it has a superb cooling capacity.it cools our all the room very fast .now it was my loving brand.</p>
 
+			<img src="../img/banner.jpg" alt="">
+			<p>Website Designer</p>
+			<span class="fa fa-star checked"></span>
+			<span class="fa fa-star checked"></span>
+			<span class="fa fa-star checked"></span>
+			<span class="fa fa-star checked"></span>
+			<span class="fa fa-star checked"></span>
+			<p>We have using ac brands of all types but it is my first time with hitach 1.5 ton it is a mind blowing air conditioner it has a superb cooling capacity.it cools our all the room very fast .now it was my loving brand.</p>
 
-
-
+			<img src="../img/banner.jpg" alt="">
+			<p>Logo Designer</p>
+			<span class="fa fa-star checked"></span>
+			<span class="fa fa-star checked"></span>
+			<span class="fa fa-star checked"></span>
+			<span class="fa fa-star checked "></span>
+			<span class="fa fa-star "></span>
+			<p>We have using ac brands of all types but it is my first time with hitach 1.5 ton it is a mind blowing air conditioner it has a superb cooling capacity.it cools our all the room very fast .now it was my loving brand.</p>
 		</div>
 
+		<div class="col-md-6 company-info pb-20 pl-80 pr-20 ">
+			<h3><u>You Can Also Check-</u></h3>
+			<img src="../img/banner.jpg" alt="" >
+			<h3><strong>Dummy India Pvt. Ltd.</strong></h3>
+			<p><strong>Location-Delhi</strong><span class="pl-10">250 Profile Views</span></p>
+			<p><span> Nature of Business-Dealer</span></p>
+			<p><span class="pl-10"> Product Dealing-Compressor,Piston,Tools</span></p>
+			<p> <span class="pl-10"> Brand Company-Usha,Algi,Shakti,Kirlooskar</span></p><br>
 
-		<?php } ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		<div class="height30"></div>
-		<div class="row">
+			<img src="../img/banner.jpg" alt="" >
+			<h3><strong>Dummy India Pvt. Ltd.</strong></h3>
+			<p><strong>Location-Delhi</strong><span class="pl-10">250 Profile Views</span></p>
+			<p><span> Nature of Business-Dealer</span></p>
+			<p><span class="pl-10"> Product Dealing-Compressor,Piston,Tools</span></p>
+			<p> <span class="pl-10"> Brand Company-Usha,Algi,Shakti,Kirlooskar</span></p><br>
 
 
+			<img src="../img/banner.jpg" alt="" >
+			<h3><strong>Dummy India Pvt. Ltd.</strong></h3>
+			<p><strong>Location-Delhi</strong><span class="pl-10">250 Profile Views</span></p>
+			<p><span> Nature of Business-Dealer</span></p>
+			<p><span class="pl-10"> Product Dealing-Compressor,Piston,Tools</span></p>
+			<p> <span class="pl-10"> Brand Company-Usha,Algi,Shakti,Kirlooskar</span></p><br>
 
 
 
 		</div>
 
 	</div>
-</div>
+				</div>
+		</div>
 
 
 
