59d8b399ebee825da5f8aabc9ffd3809957e21f0
bishwajeetgupta <50728266+bishwajeetgupta@users.noreply.github.com>
2019-05-22 17:25:01 +0530

    BLOG PAGE IMPEMENT
    "implement done"

--- local/resources/views/about.blade.php
+++ local/resources/views/about.blade.php
@@ -39,26 +39,62 @@
 	<div class="video">
 	<div class="clearfix"></div>
 	<div class="headerbg">
-	 <div class="col-md-12" align="center"><h1><?php echo $about[0]->page_title;?></h1></div>
+	 <div class="col-md-12" align="center"><h1>Blog</h1></div>
 	 </div>
+	</div>
+
+
 	<div class="container">
-	
-	 <div class="height30"></div>
-	 <div class="row">
-	<div class="col-md-12">
-	
-	
-	
-	
-	<?php echo str_replace("'","",$about[0]->page_desc);?>
-	</div>
-	
-	
-	
-	
-	</div>
-	
-	</div>
+
+
+
+
+		<div class="row">
+			<div class="clearfix"></div>
+
+			<div class="col-md-12 ">
+
+
+				<div class="row test">
+					<div class="row review-point m-0">
+						<div class="col-md-2 blog-img">
+							<img src="http://localhost/dealersahab/local/images/shop/1496146095.jpg" alt="">
+						</div>
+						<div class="col-md-10 blog-info ">
+							<a href="http://localhost/dealersahab/vendor/wpchecking"><h3><strong>Wine Shop</strong></h3></a>
+							<p>Location-42, Featherstone Street LONDON EC1Y 8SY UNITED KINGDOM<span class="pl-10">250 Profile Views</span></p>
+							<p>  Nature of Business-Dealer</p>
+							<p>Product Dealing-Compressor,Piston,Tools</p>
+							<p> Brand Company-Usha,Algi,Shakti,Kirlooskar</p>
+							<a href="#" class="btn btn-success float-right blog-more-btn">Read more</a>
+						</div>
+					</div><hr>
+					<div class="row review-point m-0">
+						<div class="col-md-2 blog-img">
+							<img src="http://localhost/dealersahab/local/images/shop/1496146095.jpg" alt="">
+						</div>
+						<div class="col-md-10 blog-info ">
+							<a href="http://localhost/dealersahab/vendor/wpchecking"><h3><strong>Wine Shop</strong></h3></a>
+							<p>Location-42, Featherstone Street LONDON EC1Y 8SY UNITED KINGDOM<span class="pl-10">250 Profile Views</span></p>
+							<p>  Nature of Business-Dealer</p>
+							<p>Product Dealing-Compressor,Piston,Tools</p>
+							<p> Brand Company-Usha,Algi,Shakti,Kirlooskar</p>
+							<a href="#" class="btn btn-success float-right blog-more-btn">Read more</a>
+						</div>
+					</div><hr>
+
+				</div>
+
+
+
+			</div>
+
+
+
+		</div>
+
+
+
 	</div>
 	
 	
