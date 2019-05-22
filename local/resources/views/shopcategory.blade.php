25a0112694fa4e680642f0eb54f6e8ceac6b75d4
nitinjavnic <nitin.javnic@gmail.com>
2019-05-22 11:27:39 +0530

    "work on filter"

---
+++ local/resources/views/shopcategory.blade.php
@@ -0,0 +1,44 @@
+
+    <div class="row review-point m-0">
+        <div class="col-md-7 company-info ">
+            <img src="http://localhost/dealerSahab/local/images/shop/1496146095.jpg" alt="">
+            <a href="http://localhost/dealerSahab/vendor/wpchecking"><h3><strong>Wine Shop</strong></h3></a>
+            <p>Location-42, Featherstone Street LONDON EC1Y 8SY UNITED KINGDOM<span class="pl-10">250 Profile Views</span></p>
+            <p>  Nature of Business-Dealer</p>
+            <p>Product Dealing-Compressor,Piston,Tools</p>
+            <p> Brand Company-Usha,Algi,Shakti,Kirlooskar</p><br></div>
+        <div class="col-md-5">
+            <div class="well well-sm">
+                <div class="row">
+                    <div class="col-xs-12 col-md-6 text-center">
+                        <h1 class="rating-num">
+                            3									</h1>
+                        <div class="rating">
+
+                            <img src="http://localhost/dealerSahab/local/images/3star.png" alt="rated 3													stars" class="star_rates">
+                        </div>
+                        <div>
+                            <span class="glyphicon glyphicon-user"></span>47 Rating
+                            <button type="button" class="btn btn-warning" name="submit">View Detail</button>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-6">
+                        <div class="row rating-desc">
+                            <div class="col-xs-3 col-md-3 text-right">
+                                <span class="glyphicon glyphicon-star"></span>3										</div>
+                            <div class="col-xs-8 col-md-9">
+                                <div class="progress progress-striped">
+                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+                                        <span class="sr-only">80%</span>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <!-- end 1 -->
+                        </div>
+                        <!-- end row -->
+                    </div>
+                </div>
+            </div>
+        </div></div>
