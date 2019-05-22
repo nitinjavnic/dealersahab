7c1836df57630c37d0ea039dd00b257b16e4ea53
bishwajeetgupta <50728266+bishwajeetgupta@users.noreply.github.com>
2019-05-22 17:27:32 +0530

    Update vendor.blade.php
    gh

--- local/resources/views/vendor.blade.php
+++ local/resources/views/vendor.blade.php
@@ -2,16 +2,16 @@
 <html lang="en">
 <head>
 
-	<?php
+    <?php
 
-	$url = URL::to("/"); ?>
+    $url = URL::to("/"); ?>
 
-	@include('style')
-
-
+    @include('style')
 
 
-	<script src="<?php echo $url;?>/js/lightbox-plus-jquery.min.js"></script>
+
+
+    <script src="<?php echo $url;?>/js/lightbox-plus-jquery.min.js"></script>
 </head>
 <body>
 
@@ -38,169 +38,73 @@
 
 
 <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12 col-12 bg-company-info">
+    <div class="row">
+        <div class="col-md-12 col-12 bg-company-info">
 
-	</div>
-</div>
-
-		<div class="container block-company">
-				<div class="row ">
-					<div class="col-md-2 col-4">
-						<img src="../img/banner.jpg" class="img-fluid company-profile"></div>
-					<div class="col-md-6 col-8">
-						<h2>Dummy India pvt ltd.</h2>
-						<p><strong>Location-</strong> KirtiNagar, Delhi 200 <strong>Profile View</strong></p>
-						<table class="text-center">
-							<tr>
-								<td><strong>Nature of Business</strong></td>
-								<td><strong>Year of Estblishment</strong></td>
-							</tr>
-							<tr>
-								<td>Manufecturer</td>
-								<td>2010</td>
-								<td></td>
-							</tr>
-							<tr>
-								<td><strong>legar status of Firm</strong></td>
-								<td><strong>GST NO.</strong></td>
-							</tr>
-							<tr>
-								<td>Manufecturer</td>
-								<td>2010</td>
-								<td></td>
-							</tr>
-						</table>
-							<p>Concise description which, among other items of information, includes (1) firm's history, (2) number and quality of its human, financial, and physical resources (3) organizational and management structure, (4) past, current and anticipated performance, and (5) its reputation, and the standing of its goods or services.</p>
-					</div>
-					<div class="col-md-4 col-12">
-						<div class="well well-sm">
-							<div class="row">
-								<div class="col-xs-12 col-md-6 text-center">
-									<h1 class="rating-num">
-										4									</h1>
-									<div class="rating">
-
-										<img src="http://localhost/dealersahab/local/images/4star.png" alt="rated 4													stars" class="star_rates">
-									</div>
-									<div>
-										<span class="glyphicon glyphicon-user"></span>47 Rating
-										<button type="button" class="btn btn-warning" name="submit">View Detail</button>
-									</div>
-								</div>
-								<div class="col-xs-12 col-md-6">
-									<div class="row rating-desc">
-										<div class="col-xs-3 col-md-3 text-right">
-											<span class="glyphicon glyphicon-star"></span>4										</div>
-										<div class="col-xs-8 col-md-9">
-											<div class="progress progress-striped">
-												<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-													<span class="sr-only">80%</span>
-												</div>
-
-											</div>
-										</div>
-
-										<!-- end 1 -->
-									</div>
-									<!-- end row -->
-								</div>
-							</div>
-						</div>
-						<p><strong>Product Dealing-</strong> Compressor,ac Freez Motor</p>
-						<p><strong>Brand Comoany-</strong> Alg8i, Usha</p>
-					</div>
-
-
-
-
-
-
-
-
-	<div class="row">
-		<div class="col-md-8 company-info pb-20 pl-80 pr-20">
-			<h3><span class="pl-80">Reviews</span></h3>
-			<img src="../img/banner.jpg" alt="">
-			<p>Graphic Designer</p>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star "></span>
-			<span class="fa fa-star "></span>
-			<p>We have using ac brands of all types but it is my first time with hitach 1.5 ton it is a mind blowing air conditioner it has a superb cooling capacity.it cools our all the room very fast .now it was my loving brand.</p>
-
-			<img src="../img/banner.jpg" alt="">
-			<p>Logo Maker</p>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star "></span>
-			<p>We have using ac brands of all types but it is my first time with hitach 1.5 ton it is a mind blowing air conditioner it has a superb cooling capacity.it cools our all the room very fast .now it was my loving brand.</p>
-
-			<img src="../img/banner.jpg" alt="">
-			<p>Website Designer</p>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<p>We have using ac brands of all types but it is my first time with hitach 1.5 ton it is a mind blowing air conditioner it has a superb cooling capacity.it cools our all the room very fast .now it was my loving brand.</p>
-
-			<img src="../img/banner.jpg" alt="">
-			<p>Logo Designer</p>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked "></span>
-			<span class="fa fa-star "></span>
-			<p>We have using ac brands of all types but it is my first time with hitach 1.5 ton it is a mind blowing air conditioner it has a superb cooling capacity.it cools our all the room very fast .now it was my loving brand.</p>
-		</div>
-
-		<div class="col-md-4 company-info">
-			<h3><u>You Can Also Check-</u></h3>
-			<img src="../img/banner.jpg" alt="" >
-			<h3><strong>Dummy India Pvt. Ltd.</strong></h3>
-			<p><strong>Location-Delhi</strong><span class="pl-10">250 Profile Views</span></p>
-			<p><span> Nature of Business-Dealer</span></p>
-			<p><span class="pl-10"> Product Dealing-Compressor,Piston,Tools</span></p>
-			<p> <span class="pl-10"> Brand Company-Usha,Algi,Shakti,Kirlooskar</span></p><br>
-
-			<img src="../img/banner.jpg" alt="" >
-			<h3><strong>Dummy India Pvt. Ltd.</strong></h3>
-			<p><strong>Location-Delhi</strong><span class="pl-10">250 Profile Views</span></p>
-			<p><span> Nature of Business-Dealer</span></p>
-			<p><span class="pl-10"> Product Dealing-Compressor,Piston,Tools</span></p>
-			<p> <span class="pl-10"> Brand Company-Usha,Algi,Shakti,Kirlooskar</span></p><br>
-
-
-			<img src="../img/banner.jpg" alt="" >
-			<h3><strong>Dummy India Pvt. Ltd.</strong></h3>
-			<p><strong>Location-Delhi</strong><span class="pl-10">250 Profile Views</span></p>
-			<p><span> Nature of Business-Dealer</span></p>
-			<p><span class="pl-10"> Product Dealing-Compressor,Piston,Tools</span></p>
-			<p> <span class="pl-10"> Brand Company-Usha,Algi,Shakti,Kirlooskar</span></p><br>
-
-
-
-		</div>
+        </div>
     </div>
-                </div>
-		</div>
+
+    <div class="container block-company">
+        <div class="row ">
+            <div class="col-md-2 col-4">
+
+                <?php $shopphoto="/shop/";
+                $paths ='/local/images'.$shopphoto.$shop[0]->profile_photo;
+                if($shop[0]->profile_photo!=""){?>
+                <img src="<?php echo $url.$paths;?>" class="img-fluid company-profile"></div>
+
+            <?php } else { ?>
+
+            <img src="<?php echo $url.'/local/images/nophoto.jpg';?>" class="img-fluid company-profile"></div>
 
 
+        <?php } ?>
 
 
+        <div class="col-md-6 col-8">
+            <h2><?php echo $shop[0]->shop_name;?></h2>
+            <p><strong>Location-</strong> <?php echo $shop[0]->address;?> <strong>Profile View</strong></p>
+            <table class="text-center">
+                <tr>
+                    <td><strong>Nature of Business</strong></td>
+                    <td><strong>Year of Estblishment</strong></td>
+                </tr>
+                <tr>
+                    <td><?php echo $sellertype; ?></td>
+                    <td>2010</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><strong>legar status of Firm</strong></td>
+                    <td><strong>GST NO.</strong></td>
+                </tr>
+                <tr>
+                    <td>Manufecturer</td>
+                    <td>2010</td>
+                    <td></td>
+                </tr>
+            </table>
+            <p>Concise description which, among other items of information, includes (1) firm's history, (2) number and quality of its human, financial, and physical resources (3) organizational and management structure, (4) past, current and anticipated performance, and (5) its reputation, and the standing of its goods or services.</p>
+        </div>
+        <div class="col-md-4 col-12">
 
 
+            <?php foreach($rating as $newrating){?>
 
-@include('footer')
-<?php if(session()->has('message')){?>
-<script type="text/javascript">
-	alert("<?php echo session()->get('message');?>");
-</script>
-</div>
-<?php } ?>
-</body>
-</html>
+            <div class="well well-sm">
+                <div class="row">
+                    <div class="col-xs-12 col-md-6 text-center">
+                        <h1 class="rating-num">
+                            <?php echo $newrating->rating; ?>
+                        </h1>
+
+
+                        <div class="rating">
+
+<?php
+if($newrating->rating=="")
+{
+    $starpath = '/local/images/nostar.png';
+}
+else {
+    $starpath
