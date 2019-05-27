66b75c0b70d85938181b37010bf88ccb5459bf89
nitinjavnic <nitin.javnic@gmail.com>
2019-05-27 18:04:10 +0530

    "image filter"

--- local/resources/views/footer.blade.php
+++ local/resources/views/footer.blade.php
@@ -31,11 +31,11 @@ $setid=1;
 			<div class=" center-block pb-10">
 				<br />
 				<a href="<?php echo $setts[0]->site_facebook;?>"><i id="social-gp" class="fa fa-facebook fa-2x social"></i></a>
-				<a href="#"><i id="social-gp" class="fa fa-twitter fa-2x social"></i></a>
+				<a href="<?php echo $setts[0]->site_twitter;?>"><i id="social-gp" class="fa fa-twitter fa-2x social"></i></a>
 				<a href="#"><i id="social-gp" class="fa fa-linkedin fa-2x social"></i></a>
 				<a href="#"><i id="social-gp" class="fa fa-instagram fa-2x social"></i></a>
 				<a href="#"><i id="social-gp" class="fa fa-youtube fa-2x social"></i></a>
-				<a href="#"><i id="social-gp" class="fa fa-pinterest-p fa-2x social"></i></a>
+				<a href="<?php echo $setts[0]->site_pinterest;?>"><i id="social-gp" class="fa fa-pinterest-p fa-2x social"></i></a>
 			</div>
 		
 		</div>
