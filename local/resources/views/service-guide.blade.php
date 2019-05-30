26e2d2f3997fb78cb9195b0cb08fe52b85638f48
bishwajeetgupta <50728266+bishwajeetgupta@users.noreply.github.com>
2019-05-30 15:12:11 +0530

    message type
    message type

--- local/resources/views/service-guide.blade.php
+++ local/resources/views/service-guide.blade.php
@@ -45,21 +45,25 @@
 	
 	 <div class="height30"></div>
 	 <div class="row">
-	<div class="col-md-12">
-
-		<h5>User Name</h5>
-		<p>hello</p>
+		 <div class="col-md-1"></div>
+		 <div class="col-md-10 ">
+			 <div class="border-shadow p-20">
+		<h4>User Name</h4>
+		<p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
 		<hr/>
+		<p class="text-info">Attachment File:- </p>
+		 </div>
+		 </div>
+		 <div class="col-md-1"></div>
+	</div>
+	
+	
+	
+	
+	</div>
+	
+	</div>
 
-	</div>
-	
-	
-	
-	
-	</div>
-	
-	</div>
-	</div>
 	
 	
 	
