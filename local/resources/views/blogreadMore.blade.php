a5ac72e561e5260bc058a20004ff79d953b4e594
bishwajeetgupta <50728266+bishwajeetgupta@users.noreply.github.com>
2019-06-14 17:54:38 +0530

    blog design
    blog design

--- local/resources/views/blogreadMore.blade.php
+++ local/resources/views/blogreadMore.blade.php
@@ -32,8 +32,8 @@
 
 
             <div class="row review-point m-0">
-                <div class="col-md-4 blog-img">
-                    <div class="view-blog">
+                <div class="col-md-4 blog-img ">
+
 
                     <?php
                     $servicephoto="/blogphoto/";
@@ -53,35 +53,34 @@
                         <a href="#"><i id="social-gp" class="fa fa-linkedin-square fa-2x social"></i></a>
                         <a href="#"><i id="social-gp" class="fa fa-quora fa-2x social"></i></a>
                     </div>
+                        <hr/>
+                        <div class="fancy blog-description">
+                            <h2 class=" text-center">Business Rating</h2>
+                            <div class="p-25">
+                                <p><a href="#">Online Factor</a></p>
+                                <p><a href="#">Hello</a></p>
+                                <p><a href="#">Hello</a></p>
+                                <p><a href="#">Hello</a></p>
+                                <p><a href="#">Hello</a></p>
+                                <p><a href="#">Hello</a></p>
+                                <p><a href="#">Hello</a></p>
+                                <p><a href="#">Hello</a></p>
+                                <p><a href="#">Hello</a></p>
 
-                    </div>
+                            </div>
+                        </div>
+
                 </div>
 
 
 
-                <div class="col-md-6 blog-info ">
+                <div class="col-md-8 blog-info ">
                   <h3 class="mt-0 text-capitalize text-info"><strong><?php echo $newblog->blog_titile; ?></strong></h3>
                     <p><?php echo $newblog->full_description; ?></p>
 
                 </div>
 
-                <div class="col-md-2 padd-0  blog-read-heading ">
-                  <div class="view-blog border-shadow">
-                    <h3 class="filter-heading text-center">Business Rating</h3>
-                <div class="pl-10 ">
-                    <p><a href="#">Online Factor</a></p>
-                    <p><a href="#">Hello</a></p>
-                    <p><a href="#">Hello</a></p>
-                    <p><a href="#">Hello</a></p>
-                    <p><a href="#">Hello</a></p>
-                    <p><a href="#">Hello</a></p>
-                    <p><a href="#">Hello</a></p>
-                    <p><a href="#">Hello</a></p>
-                    <p><a href="#">Hello</a></p>
 
-                </div>
-                </div>
-                </div>
             </div>
 
         </div>
